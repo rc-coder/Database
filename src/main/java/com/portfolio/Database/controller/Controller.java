9beedffe70c385ac5f9bd54ac40754cd3c150ae2
rc-coder <rcrd.campelo@gmail.com>
2023-05-14 19:08:47 -0300

    error con la implementacion de cors

--- src/main/java/com/portfolio/Database/controller/Controller.java
+++ src/main/java/com/portfolio/Database/controller/Controller.java
@@ -8,7 +8,6 @@ import com.portfolio.Database.service.IProyectoService;
 import com.portfolio.Database.service.ITecnologiaService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -19,7 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@CrossOrigin(origins = "http://localhost:4200")
+
 @RestController
 @RequestMapping("/api/db")
 public class Controller {
